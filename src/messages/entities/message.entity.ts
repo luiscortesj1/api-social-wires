aad51094c9e2dd6cc9fa19da3c286026a1b24d82
Luis Gabriel Cortes Jacanamijoy <61705467+luiscortesj1@users.noreply.github.com>
2023-03-12 20:08:28 -0500

    Update message.entity.ts

--- src/messages/entities/message.entity.ts
+++ src/messages/entities/message.entity.ts
@@ -10,27 +10,18 @@ import {
 import { Comment } from './Comment.entity';
 import { Reaction } from './Reaction.entity';
 
-//import { Comment } from './comment.entity';
-//import { Reaction } from './reaction.entity';
-import { ApiProperty } from '@nestjs/swagger';
 
 @Entity()
 export class Message {
-  @ApiProperty()
+  
   @PrimaryGeneratedColumn()
   id: number;
-  @ApiProperty()
-  @ApiProperty()
   @Column('text')
   title: string;
-
-  @ApiProperty()
   @Column('text')
   content: string;
-  @ApiProperty()
   @Column('number')
   user_id: number;
-  @ApiProperty()
   @Column({ type: 'boolean', default: true })
   active: boolean;
 
